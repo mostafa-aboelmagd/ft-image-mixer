407d8e5bcd6fba9d54d2c7a1289875e0c7679c00
Mosafa Ayman <mostafaaboelmagd01@gmail.com>
2024-12-17 01:22:29 +0200

    Completed Changing Brightness & Contrast Of Browsed Image

--- main.py
+++ main.py
@@ -13,29 +13,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.show()
     
     def setupVariables(self):
-        self.minWidth = 300
-        self.minHeight = 300
         self.imageContainers = [self.original1, self.original2, self.original3, self.original4]
     
     def addEventListeners(self):
-        # for img in self.imageContainers:
-        #     img.isBrowsed.connect(self.uniformSize)
         pass
-
-    # def uniformSize(self):
-    #     for img in self.imageContainers:
-    #         if img.width != 0:
-    #             if (self.minWidth == 0 or (img.width * img.height < self.minWidth * self.minHeight)):
-    #                 self.minWidth = img.width
-    #                 self.minHeight = img.height
-                    
-
-    #     if self.minWidth != 0:    
-    #         for img in self.imageContainers:
-    #             if img.width != 0 and img.uploaded:
-    #                 img.width = self.minWidth 
-    #                 img.height = self.minHeight 
-    #                 img.addImage()
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
